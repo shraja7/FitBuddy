8f37666f49cda323b3b97e3df5f280a7866c5aa2
shraja7 <shraja7@gmail.com>
2023-03-17 15:35:36 -0500

    set up back end connection w mongo atlas

---
+++ FitBuddy/backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
